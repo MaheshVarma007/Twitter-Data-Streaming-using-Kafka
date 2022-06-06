e8bf6c15676fa3d250e3af2b8125bb3f8e6adee0
Mahesh Varma <k.maheshvarma123@gmail.com>
2022-06-06 12:43:58 +0530

    first Commit

---
+++ src/main/java/com/example/demo/producer/TwitterConfig.java
@@ -0,0 +1,9 @@
+package com.example.demo.producer;
+
+public class TwitterConfig {
+    public static final String CONSUMER_KEYS= "0X86aVxCLURVqvqsq7y4784F0";
+    public static final String CONSUMER_SECRETS= "OWLSfy44Jvp7KFMpdLeQzSWJKWnOjInF80H9NOPL0IhAzFGkdA";
+    public static final String SECRET = "4mnlxDhdck8VVZFfNAgoJtVLHtR5cNgxVbjpzDohEqFgz";
+    public static final String TOKEN = "848234762330189824-CtwgFCHZihSK0Uqx19bDcUhLzYiZMbc";
+
+}
