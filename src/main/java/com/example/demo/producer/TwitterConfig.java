d51ff0e434589f7f8da6468dd6bde2d69206519e
Mahesh Varma <70755190+MaheshVarma007@users.noreply.github.com>
2022-06-06 12:45:49 +0530

    Update TwitterConfig.java

--- src/main/java/com/example/demo/producer/TwitterConfig.java
+++ src/main/java/com/example/demo/producer/TwitterConfig.java
@@ -1,9 +1,9 @@
 package com.example.demo.producer;
 
 public class TwitterConfig {
-    public static final String CONSUMER_KEYS= "0X86aVxCLURVqvqsq7y4784F0";
-    public static final String CONSUMER_SECRETS= "OWLSfy44Jvp7KFMpdLeQzSWJKWnOjInF80H9NOPL0IhAzFGkdA";
-    public static final String SECRET = "4mnlxDhdck8VVZFfNAgoJtVLHtR5cNgxVbjpzDohEqFgz";
-    public static final String TOKEN = "848234762330189824-CtwgFCHZihSK0Uqx19bDcUhLzYiZMbc";
+    public static final String CONSUMER_KEYS= "0X**********F0";
+    public static final String CONSUMER_SECRETS= "OW**********dA";
+    public static final String SECRET = "4m***********gz";
+    public static final String TOKEN = "84**********bc";
 
 }
